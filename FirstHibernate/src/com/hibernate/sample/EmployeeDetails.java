ee9479a432cc7d16a17aaa45610815bd1cab4417
govind <govindchaurasia1989@gmail.com>
2017-03-03 19:51:43 +0530

    @Embedded Hibernate

--- FirstHibernate/src/com/hibernate/sample/EmployeeDetails.java
+++ FirstHibernate/src/com/hibernate/sample/EmployeeDetails.java
@@ -1,24 +1,62 @@
 package com.hibernate.sample;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-@Entity
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity (name="Employee_details")
+// @Table (name="Employee")
 public class EmployeeDetails {
-	@Id
+	@Id @GeneratedValue (strategy=GenerationType.AUTO)
 	private int eid;
+	@Column (name="employeeName")
 	private String name;
 	private String contacts;
+//	@Temporal (TemporalType.DATE) -- Enters only date in currentdate field
+//	@Transient  -- Skip this fields
+	private Date currentDate;
+//	@Lob -- If large amount of data is stored i.e cross the limit of 255 character which is by default 
+	private String description;
 	
-	public EmployeeDetails() {
-		
-	}
-	public EmployeeDetails(int eid, String name, String contacts) {
-		this.eid = eid;
-		this.name = name;
-		this.contacts = contacts;
-	}
+	@ElementCollection
+	@JoinTable(name="address",
+			joinColumns=@JoinColumn(name="eid"))
+	private Set<Address> listOfAddress = new HashSet<Address>(); 
+			
 	
+	public Set<Address> getListOfAddress() {
+		return listOfAddress;
+	}
+	public void setListOfAddress(Set<Address> listOfAddress) {
+		this.listOfAddress = listOfAddress;
+	}
+	public Date getCurrentDate() {
+		return currentDate;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setCurrentDate(Date currentDate) {
+		this.currentDate = currentDate;
+	}
+
 	public int getEid() {
 		return eid;
 	}
--- FirstHibernate/src/com/hibernate/worker/EmployeeWorker.java
+++ FirstHibernate/src/com/hibernate/worker/EmployeeWorker.java
@@ -1,11 +1,14 @@
 package com.hibernate.worker;
 
-import org.hibernate.HibernateException; 
+
+import java.util.Date;
+
 import org.hibernate.Session; 
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.hibernate.sample.Address;
 import com.hibernate.sample.EmployeeDetails;
 
 public class EmployeeWorker {
@@ -13,20 +16,43 @@ public class EmployeeWorker {
 	public static void main(String[] args) {
 		
 		EmployeeDetails emp= new EmployeeDetails();
-		emp.setEid(3);
 		emp.setName("Govind");
 		emp.setContacts("84145454");
+		emp.setCurrentDate(new Date());
+		emp.setDescription("Anything entered here");
+		
+		Address address1= new Address();
+		address1.setCity("Mumbai");
+		address1.setState("Maharashtra");
+		address1.setPincode(111014);
+		
+		Address address2= new Address();
+		address2.setCity("Mumbai");
+		address2.setState("Maharashtra");
+		address2.setPincode(111056);
+		
+		emp.getListOfAddress().add(address1);
+		emp.getListOfAddress().add(address2);
 		
 		try
 		{
-			System.out.println("		SessionFactory "+new Configuration().configure().buildSessionFactory());
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		
 		Session session=sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(emp);
 		session.getTransaction().commit();
-		}
+		session.close();
+		/*
+		emp=null;
+		
+		session=sessionFactory.openSession();
+		session.beginTransaction();
+		emp=(EmployeeDetails)session.get(EmployeeDetails.class, 1);
+		Sytem.out.println("Data fetched "+emp.getName());*/
+		
+		
+		} 
 		catch(Exception e)
 		{
 			e.printStackTrace();
